2439b6c6f996ae06b6464c97c806989fc5b8509c
SteveViss <s.vissault@yahoo.fr>
2015-02-21 18:26:20 -0500

    Significant improvements - switch program to R

---
+++ fcts_hdf.r
@@ -0,0 +1,59 @@
+check_res <- function(lon,lat){
+
+    rs = 0
+
+    if(all(dim(lat) == dim(lon)) == FALSE){
+      write("lat/lon lattices with different dimensions...", stderr())
+    }
+
+    reso <- round(get_res(lon,lat),digit=3)
+
+    if(round((range(lat)[2]-range(lat)[1])/nrow(lat),digit=3) != reso){
+        write("lat resolution is not constant...", stderr())
+        rs = rs +1
+    }
+
+    if(round((range(lon)[2]-range(lon)[1])/ncol(lon),digit=3) != reso){
+        write("lon resolution is not constant...", stderr())
+        rs = rs +1
+    }
+
+    return(rs)
+}
+
+get_res <- function(lon,lat){
+
+
+    resx <- lon[nrow(lon),2] - lon[nrow(lon),1]
+    resy <- lat[nrow(lat)-1,1] - lat[nrow(lat),1]
+
+    if(round(resx,digit=6) == round(resy,digit=6)){
+        return(resx)
+    } else {
+        warning("raster resolution differs...")
+        return(list("resx"=resx,"resy"=resy))
+    }
+}
+
+get_ext <- function(lon,lat){
+    ext <- extent(as.numeric(c(range(lon),range(lat))))
+    return(ext)
+}
+
+write_stack_dates <- function(arr_var,dates=dates,ext=ext,out_path=out_path){
+
+    st <- stack()
+
+    for (t in 1:length(dates)){
+        rs <- raster(as.matrix(arr_var[t,,]))
+        extent(rs) <- ext
+        projection(rs) <- CRS("+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs ")
+        st <- addLayer(st,rs)
+    }
+
+    names(st) <- dates
+
+
+    invisible(writeRaster(stack(st), paste0(out_path,names(arr_var),"-",hfile,"-",str_replace(str_replace(names(st),".","-"),"X",""),".tif"), bylayer=TRUE, format='GTiff',overwrite=TRUE))
+
+}
---
+++ imp_monthly_hdf.r
@@ -0,0 +1,61 @@
+# The MIT License (MIT)
+#
+# Copyright (c) 2015 Steve Vissault
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+
+##### DESCRIPTION
+# Load GCMs downscaled (HDF5 files) and provided by Ouranos into a postgreSQL database
+
+
+# source("http://bioconductor.org/biocLite.R")
+# biocLite("rhdf5")
+
+rm(list=ls())
+
+require(rhdf5)
+require(rgdal)
+require(raster)
+require(stringr)
+
+source("fcts_hdf.r")
+
+hfile <- "monthlyNRCAN+CSIRO_QCCCE-CSIRO_Mk3_6_0-r1i1p1_historical+rcp60-190001-190912"
+out_path <- "./out_files/"
+
+path_hfile<- paste0("./mat_files/",hfile,".mat")
+content <- h5ls(path_hfile)
+
+lat <- h5read(path_hfile,"/out/lat")$data
+lon <- h5read(path_hfile,"/out/lon")$data
+times <- h5read(path_hfile,"/out/time_vectors")$data
+ext <- get_ext(lon,lat)
+vec_obj_vars <- list( tasmin=h5read(path_hfile,"/out/tasmin")$data,
+                        tasmax=h5read(path_hfile,"/out/tasmax")$data,
+                        pr=h5read(path_hfile,"/out/pr")$data)
+
+dates <- as.Date(paste(times[,1],times[,2],times[,3],sep="-"))
+
+if(check_res(lon,lat)>0){
+    stop("Program stopped by raster errors...",call.=TRUE)
+}
+
+invisible(lapply(vec_obj_vars,write_stack_dates,dates,ext,out_path))
+
