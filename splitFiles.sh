f9a82dfc668a808e8a85481c768ad272bf91a63b
SteveViss <s.vissault@yahoo.fr>
2015-02-26 10:45:32 -0500

    Split/unsplit files done;

--- splitFiles.sh
+++ splitFiles.sh
@@ -4,7 +4,7 @@
 
 # Checking input...
 
-read -p "Create folder by Models Y/N:" optSplit
+read -p "Split or unsplit by models (split/unsplit):" optSplit
 
 if [ -z "$1" ]; then
    echo "Please specify a directory to split, as a parameter to the script."
@@ -19,30 +19,20 @@ fi
 # set variables
 cd $1
 
-if [ $optSplit == 'Y' ] || [ $optSplit == 'y' ]; then
+if [ $optSplit == 'Split' ] || [ $optSplit == 'split' ]; then
     files=(`ls *.mat | sed -e 's/.\{18\}$//' | uniq`)
     echo 'Number of GCMs:' ${#files[*]}
 
     for uqfile in ${files[*]}; do
        mkdir -p $uqfile
        echo $uqfile$ext
-       find . -maxdepth 1 -name $uqfile'*' -exec mv {} $uqfile/ \;
+       find -maxdepth 1 -name $uqfile'*' -exec mv {} $uqfile/ \;
     done
     exit
 else
-  if [ $optSplit == 'N' ] || [ $optSplit == 'n' ]; then
-    read -p "Number of folders:" nfold
-    files=(`ls *.mat `)
-    nfilesByFold=$(( ${#files[*]} / $nfold ))
-    mod=$(( ${#files[*]} % $nfold ))
-
-    for folder in $(seq 1 $nFold)
-      do
-         pos=$(( $pos - 1 ))
-         mkdir -p rep$folder
-         mv ${files[*]:$pos:$nfilesByFold} rep$folder
-      done
-
+  if [ $optSplit == 'Unsplit' ] || [ $optSplit == 'unsplit' ]; then
+    find . -name '*' -type f -exec mv -f {} . \;
+    find -mindepth 1 -maxdepth 1 -type d -exec rm -r {} \;
   fi
 fi
 
