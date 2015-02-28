4acae7d66be1eb33093ec3d8e28d4bf060868f9b
SteveViss <s.vissault@yahoo.fr>
2015-02-28 18:14:38 -0500

    add script to install dependencies

---
+++ install_dep.r
@@ -0,0 +1,7 @@
+# install dependancies
+
+if(!require(rhdf5)){install.packages('rhdf5',repos='http://cran.skazkaforyou.com/')}
+if(!require(rgdal)){install.packages('rgdal',repos='http://cran.skazkaforyou.com/')}
+if(!require(raster)){install.packages('raster',repos='http://cran.skazkaforyou.com/')}
+if(!require(argparse)){install.packages('argparse',repos='http://cran.skazkaforyou.com/')}
+if(!require(stringr)){install.packages('stringr',repos='http://cran.skazkaforyou.com/')}
--- prg/extract_monthly_rs.r
+++ prg/extract_monthly_rs.r
@@ -43,11 +43,6 @@ argList = parser$parse_args()
 # biocLite("rhdf5")
 
 # Librairies
-if(!require(rhdf5)){install.packages('rhdf5',repos='http://cran.skazkaforyou.com/')}
-if(!require(rgdal)){install.packages('rgdal',repos='http://cran.skazkaforyou.com/')}
-if(!require(raster)){install.packages('raster',repos='http://cran.skazkaforyou.com/')}
-if(!require(argparse)){install.packages('argparse',repos='http://cran.skazkaforyou.com/')}
-if(!require(stringr)){install.packages('stringr',repos='http://cran.skazkaforyou.com/')}
 
 require(rhdf5)
 require(rgdal)
