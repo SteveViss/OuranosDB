cc0c9d04ca0d7d6cbc20b6aeeb203dde4f407e79
SteveViss <s.vissault@yahoo.fr>
2015-02-28 18:17:32 -0500

    Fix issue with installation of package rhdf5

--- install_dep.r
+++ install_dep.r
@@ -1,6 +1,9 @@
 # install dependancies
 
-if(!require(rhdf5)){install.packages('rhdf5',repos='http://cran.skazkaforyou.com/')}
+if(!require(rhdf5)){
+    source("http://bioconductor.org/biocLite.R")
+    biocLite("rhdf5")
+}
 if(!require(rgdal)){install.packages('rgdal',repos='http://cran.skazkaforyou.com/')}
 if(!require(raster)){install.packages('raster',repos='http://cran.skazkaforyou.com/')}
 if(!require(argparse)){install.packages('argparse',repos='http://cran.skazkaforyou.com/')}
